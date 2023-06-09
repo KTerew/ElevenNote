2c135f8efed5f1dd6bb31339464198ca2b4471ef
Kody Terew <terewkody@gmail.com>
2023-06-08 20:19:01 -0400

    Step 1 Added Category|Note|User Entities

---
+++ ElevenNote.Data/ElevenNoteContext/ElevenNoteDBContext.cs
@@ -0,0 +1,47 @@
+using ElevenNote.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElevenNote.Data.ElevenNoteContext
+{
+    public class ElevenNoteDBContext : DbContext
+    {
+        public ElevenNoteDBContext(DbContextOptions options) :base(options) { }
+
+        public DbSet<NoteEntity> Notes { get; set; }
+        public DbSet<CategoryEntity> Catergory { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<CategoryEntity>().HasData(
+                new CategoryEntity
+                {
+                    Id = 1,
+                    Name = "Happy People"
+                },
+                new CategoryEntity
+                {
+                    Id = 2,
+                    Name = "Sad People"
+                },
+                new CategoryEntity
+                {
+                    Id = 3,
+                    Name = "Music"
+                },
+                new CategoryEntity
+                {
+                    Id = 4,
+                    Name = "Art"
+                },
+                new CategoryEntity
+                {
+                    Id = 5,
+                    Name = "Video"
+                }
+            );
+        }
+    }
+}
---
+++ ElevenNote.Data/Migrations/20230609000740_Init.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ElevenNote.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Catergory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Catergory", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserEntity",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NormalizedUserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NormalizedEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmailConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecurityStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    LockoutEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    AccessFailedCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserEntity", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    CreatedUTC = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    ModifiedUTC = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    OwnerId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CategoryyEntityId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Notes_Catergory_CategoryyEntityId",
+                        column: x => x.CategoryyEntityId,
+                        principalTable: "Catergory",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Notes_UserEntity_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "UserEntity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Catergory",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Happy People" },
+                    { 2, "Sad People" },
+                    { 3, "Music" },
+                    { 4, "Art" },
+                    { 5, "Video Games" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notes_CategoryyEntityId",
+                table: "Notes",
+                column: "CategoryyEntityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notes_OwnerId",
+                table: "Notes",
+                column: "OwnerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+
+            migrationBuilder.DropTable(
+                name: "Catergory");
+
+            migrationBuilder.DropTable(
+                name: "UserEntity");
+        }
+    }
+}
